9ad17dd73ff021ec8ea1a884d0493d81a832f37b
Tyler <tymitchell2100@gmail.com>
2018-12-04 10:42:18 -0500

    finished functionality

--- links/main.js
+++ links/main.js
@@ -1,9 +1,19 @@
 const game = document.querySelector('#game-wrap')
-let begSize = document.querySelector('#size').value
-let numOfMines = document.querySelector('#mineNum')
-numOfMines.value = begSize
-numOfMines.min = begSize/2
-numOfMines.max = begSize*5
+const smiley = document.querySelector('#smiley') 
+const minesCounter = document.querySelector('#mines')
+const timer = document.querySelector('#timer')
+
+function countTimer() {
+    ++totalSeconds;
+    let hour = Math.floor(totalSeconds /3600);
+    if (hour<10) hour = "0" + hour;
+    let minute = Math.floor((totalSeconds - hour*3600)/60);
+    if (minute<10) minute = "0" + minute;
+    let seconds = totalSeconds - (hour*3600 + minute*60);
+    if (seconds<10) seconds = "0" + seconds;
+ 
+    document.getElementById("timer").innerHTML = hour + ":" + minute + ":" + seconds;
+}
 
 // creates a single piece of maze
 function createBox (row,box) {
@@ -18,6 +28,7 @@ function createBox (row,box) {
     newBox.dataset.row = row;
     newBox.dataset.box = box;
     newBox.dataset.neighbors = 0;
+    newBox.dataset.empty = 0;
 }
 // creates new row
 function createRow (row) {
@@ -39,8 +50,7 @@ function placeMines (size, minesNum) {
 }
 
 // keeps track of number of neighbor mines for each box
-function placeNums (size) {
-    let boxes = document.querySelectorAll('.box')
+function placeNums (size,boxes) {
     boxes.forEach (box => {
         let relativeRow = parseInt(box.dataset.row)
         let boxIndex = parseInt(box.dataset.box)
@@ -58,25 +68,109 @@ function placeNums (size) {
     })
 }
 
-// places the images in box
-function placeImages () {
-    let boxes = document.querySelectorAll('.box')
+// runs when box is clicked
+function displayImages (e) {
+    let currentBox = e.currentTarget
+
+    if (currentBox.childElementCount>0) return
+    
+    if (currentBox.dataset.mines == 1) {
+        displayLoss()
+    } else if (currentBox.dataset.neighbors > 0) {
+        currentBox.appendChild(createImg(`links/img/${currentBox.dataset.neighbors}.png`))
+    } else {
+        displayEmpty(currentBox, 0, 1)
+    }
+    currentBox.dataset.empty = 1
+
+    let win = true
+    const boxes = document.querySelectorAll('.box')
     boxes.forEach (box => {
+        if (box.dataset.empty == 0) win=false
+    })
+    if (win) youWin()
+}
+
+function youWin () {
+    alert('you win')
+    clearTimeout(myTimer);
+}
+
+// empties surrounding empty spaces
+function displayEmpty (node, target, replacement) {
+    if (node.dataset.empty == replacement) return
+    if (node.dataset.neighbors != target) return
+    node.appendChild(createImg('links/img/tile.png'))
+    node.dataset.empty = 1
+
+    let size = document.querySelector('#size').value
+    let relativeRow = parseInt(node.dataset.row)
+    let boxIndex = parseInt(node.dataset.box)
+    for (let i = relativeRow-1; i<=relativeRow+1; i++) {
+        for (let j = boxIndex-1; j<=boxIndex+1; j++) {
+            if (j<0 || i<0 || j>=size || i>=size) continue
+            if (i == relativeRow && j == boxIndex) continue
+
+            let neighborBox = document.getElementById(`[${i}][${j}]`)
+
+            displayEmpty(neighborBox, target, replacement)
+        }
+    }
+    return
+}
+
+function displayLoss() {
+    const boxes = document.querySelectorAll('.box')
+    boxes.forEach (box => {
+        box.removeEventListener('click', displayImages)
+        box.removeEventListener('contextmenu', displayFlag)
+
         if (box.dataset.mines == 1) {
-            box.innerHTML = 'B'
-        } else if (box.dataset.neighbors == 0) {
-            box.innerHTML = ''
-        } else {
-            box.innerHTML = box.dataset.neighbors
+            if (box.childElementCount>0){
+                box.removeChild(box.lastElementChild)
+            } 
+            box.appendChild(createImg('links/img/bomb.png'))
         }
     })
+    smiley.src = "links/img/sad"
+
+    clearTimeout(myTimer);
+}
+
+function createImg (img) {
+    let image = document.createElement('img')
+    image.src = img
+    return image
+}
+
+// runs when box is clicked
+function displayFlag (e) {
+    e.preventDefault();
+
+    let currentBox = e.currentTarget;
+    let currentValue = currentBox.innerHTML;
+    let minesNum = parseInt(minesCounter.innerHTML)
+
+    if (currentValue == '') {
+        currentBox.appendChild(createImg('links/img/flag.png'));
+        minesNum--
+    }
+    else {
+        currentBox.innerHTML = '';
+        minesNum++
+    }
+
+    minesCounter.innerHTML = minesNum
+    currentBox.dataset.empty = 1
 }
 
 // builds maze when start button is clicked
 const createGame = function () {
-    game.innerHTML = ''
+    game.innerHTML = '';
     let size = document.querySelector('#size').value;
     let minesNum = document.querySelector('#mineNum').value;
+    smiley.src = "links/img/smiley"
+    minesCounter.innerHTML = minesNum;
 
     for (let row = 0; row < size; row++ ) {
         createRow(row);
@@ -85,11 +179,23 @@ const createGame = function () {
         }
     }
 
+    const boxes = document.querySelectorAll('.box')
+    boxes.forEach (box => {
+        box.addEventListener('click', displayImages)
+        box.addEventListener('contextmenu', displayFlag)
+    })
+
+    totalSeconds = 0;
+    clearInterval(myTimer);
+    myTimer = setInterval(countTimer, 1000);
+
     placeMines(size, minesNum);
-    placeNums(size)
-    placeImages()
+    placeNums(size, boxes);
 }
 
 document.querySelector('#start').addEventListener('click', function() {
-    createGame()
+    createGame();
 })
+
+let totalSeconds = 0;
+let myTimer
